ccff7f7fbabcf55995bd514c0443c5561b13a7f1
Erick Muñiz Morales <erickmuniz@ciencias.unam.mx>
2022-04-02 19:54:39 -0600

    Implemented a function to read the dataframe. It refactorizes the datetime of start session and the type name

--- workoutanalysis.py
+++ workoutanalysis.py
@@ -4,13 +4,62 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# ReadData
+def refactor_data_frame(df: 'pd.DataFrame') -> 'pd.DataFrame':
+    """
+    Function to incorporate all the modifications on the Zepp/MiFit
+    dataframe.
+    The following are the principal modifications:
+        - Refactor of the name type
+        - Modify the time value for each value
+            - Our current time is more detailed. The important information is based on minutes; no more!
+            - YYYY-MM-DD HH-MM
+    :param df: 'pd.DataFrame'
+    :return: df: 'pd.DataFrame'
+    """
+    namesOfWorkout: 'dict' = {1: "Running",
+                              16: "Freestyle",
+                              21: "Jump Rope"}
+    df['type'] = df['type'].replace(namesOfWorkout)
+    df['startTime'] = pd.to_datetime(df['startTime']).dt.tz_convert("America/Mexico_City")\
+        .apply(lambda x: x.strftime('%Y-%m-%d %H:%m'))
+
+    return df
+
 
 def read(path: 'str' = 'sport') -> 'pd.DataFrame':
+    """
+    Function to read and cast all the important information of the csv file.
+    This is the following format of Zepp/MiFit
+
+    Columns:
+         - 'type', it is the type of workout
+            - 1 -> Running
+            - 16 -> Freestyle
+            - 21 -> Jump rope
+         - 'startTime', it is the time of init. The format is 'YYYY-MM-DD HH:MM:SS'
+         - sportTime(s)', the time duration of the session
+         - 'distance(m)',
+         - 'calories(kcal)'
+
+         The following depends on the exercises type.
+         - 'minPace(/meter)',
+         - 'avgPace(/meter)',
+
+
+    :param path: str
+    :return: pd.Dataframe
+    """
+    # TODO: Add a simple function to read the unique csv file in the folder.
+    df = pd.DataFrame()
     if path == 'sport':
-        return pd.read_csv('data/sport/SPORT_1648939638346.csv')
-    return pd.read_csv(path)
+        df = pd.read_csv('data/sport/SPORT_1648939638346.csv')
+    else:
+        df = pd.read_csv(path)
+
+    refactor_data_frame(df)
+
+    return df
 
 
 if __name__ == '__main__':
-    df = read("data/sport/SPORT_1648939638346.csv")
+    pass
